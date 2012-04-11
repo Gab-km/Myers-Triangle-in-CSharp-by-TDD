cf2c65b5b355624761fea9ada8f073778b1a606f
Gab_km <the25thcromosome@gmail.com>
2012-04-12 00:26:39 +0900

    辺の長さが3,4,5のテストをテストケースにまとめる。
    diff --git a/MyersTriangle.Test/TriangleTest.cs b/MyersTriangle.Test/TriangleTest.cs
    index 3dbf01b..a752a75 100644
    --- a/MyersTriangle.Test/TriangleTest.cs
    +++ b/MyersTriangle.Test/TriangleTest.cs
    @@ -14,6 +14,7 @@ namespace MyersTriangle.Test
             [TestCase(2, 2, 1, TriangleType.Isosceles)]
             [TestCase(1, 3, 3, TriangleType.Isosceles)]
             [TestCase(4, 2, 3, TriangleType.Scalene)]
    +        [TestCase(3, 4, 5, TriangleType.Scalene)]
             public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
             {
                 var triangle = new Triangle(side1, side2, side3);
    @@ -21,13 +22,6 @@ namespace MyersTriangle.Test
             }
    
             [Test]
    -        public void Test_3と4と5を渡したら不等辺三角形と判定されること()
    -        {
    -            var triangle = new Triangle(3, 4, 5);
    -            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Scalene));
    -        }
    -
    -        [Test]
             public void Test_1と2と0を渡したら三角形でないと判定されること()
             {
                 var triangle = new Triangle(1, 2, 0);

--- MyersTriangle.Test/TriangleTest.cs
+++ MyersTriangle.Test/TriangleTest.cs
@@ -14,17 +14,11 @@ namespace MyersTriangle.Test
         [TestCase(2, 2, 1, TriangleType.Isosceles)]
         [TestCase(1, 3, 3, TriangleType.Isosceles)]
         [TestCase(4, 2, 3, TriangleType.Scalene)]
+        [TestCase(3, 4, 5, TriangleType.Scalene)]
         public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
         {
             var triangle = new Triangle(side1, side2, side3);
             Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(resutl));
-        }
-
-        [Test]
-        public void Test_3と4と5を渡したら不等辺三角形と判定されること()
-        {
-            var triangle = new Triangle(3, 4, 5);
-            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Scalene));
         }
 
         [Test]
