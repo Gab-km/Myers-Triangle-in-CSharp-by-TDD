e01a6fa8c19a7308a3c4d9a1047663d543165ad7
Gab_km <the25thcromosome@gmail.com>
2012-04-12 00:27:35 +0900

    辺の長さが1,2,0のテストをテストケースにまとめる。
    diff --git a/MyersTriangle.Test/TriangleTest.cs b/MyersTriangle.Test/TriangleTest.cs
    index a752a75..530de25 100644
    --- a/MyersTriangle.Test/TriangleTest.cs
    +++ b/MyersTriangle.Test/TriangleTest.cs
    @@ -15,6 +15,7 @@ namespace MyersTriangle.Test
             [TestCase(1, 3, 3, TriangleType.Isosceles)]
             [TestCase(4, 2, 3, TriangleType.Scalene)]
             [TestCase(3, 4, 5, TriangleType.Scalene)]
    +        [TestCase(1, 2, 0, TriangleType.Invalid)]
             public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
             {
                 var triangle = new Triangle(side1, side2, side3);
    @@ -22,13 +23,6 @@ namespace MyersTriangle.Test
             }
    
             [Test]
    -        public void Test_1と2と0を渡したら三角形でないと判定されること()
    -        {
    -            var triangle = new Triangle(1, 2, 0);
    -            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Invalid));
    -        }
    -
    -        [Test]
             public void Test_3と0と5を渡したら三角形でないと判定されること()
             {
                 var triangle = new Triangle(3, 0, 5);

--- MyersTriangle.Test/TriangleTest.cs
+++ MyersTriangle.Test/TriangleTest.cs
@@ -15,17 +15,11 @@ namespace MyersTriangle.Test
         [TestCase(1, 3, 3, TriangleType.Isosceles)]
         [TestCase(4, 2, 3, TriangleType.Scalene)]
         [TestCase(3, 4, 5, TriangleType.Scalene)]
+        [TestCase(1, 2, 0, TriangleType.Invalid)]
         public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
         {
             var triangle = new Triangle(side1, side2, side3);
             Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(resutl));
-        }
-
-        [Test]
-        public void Test_1と2と0を渡したら三角形でないと判定されること()
-        {
-            var triangle = new Triangle(1, 2, 0);
-            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Invalid));
         }
 
         [Test]
