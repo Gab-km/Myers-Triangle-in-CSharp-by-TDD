58872916054ccb53f0d249bc57ca3f768512d9ca
Gab_km <the25thcromosome@gmail.com>
2012-04-12 00:29:40 +0900

    辺の長さが1,1,2のテストをテストケースにまとめる。
    diff --git a/MyersTriangle.Test/TriangleTest.cs b/MyersTriangle.Test/TriangleTest.cs
    index f0ef25c..71f3229 100644
    --- a/MyersTriangle.Test/TriangleTest.cs
    +++ b/MyersTriangle.Test/TriangleTest.cs
    @@ -17,6 +17,7 @@ namespace MyersTriangle.Test
             [TestCase(3, 4, 5, TriangleType.Scalene)]
             [TestCase(1, 2, 0, TriangleType.Invalid)]
             [TestCase(3, 0, 5, TriangleType.Invalid)]
    +        [TestCase(1, 1, 2, TriangleType.Invalid)]
             public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
             {
                 var triangle = new Triangle(side1, side2, side3);
    @@ -24,13 +25,6 @@ namespace MyersTriangle.Test
             }
    
             [Test]
    -        public void Test_1と1と2を渡したら三角形でないと判定されること()
    -        {
    -            var triangle = new Triangle(1, 1, 2);
    -            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Invalid));
    -        }
    -
    -        [Test]
             public void Test_3と6と2を渡したら三角形でないと判定されること()
             {
                 var triangle = new Triangle(3, 6, 2);

--- MyersTriangle.Test/TriangleTest.cs
+++ MyersTriangle.Test/TriangleTest.cs
@@ -17,17 +17,11 @@ namespace MyersTriangle.Test
         [TestCase(3, 4, 5, TriangleType.Scalene)]
         [TestCase(1, 2, 0, TriangleType.Invalid)]
         [TestCase(3, 0, 5, TriangleType.Invalid)]
+        [TestCase(1, 1, 2, TriangleType.Invalid)]
         public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
         {
             var triangle = new Triangle(side1, side2, side3);
             Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(resutl));
-        }
-
-        [Test]
-        public void Test_1と1と2を渡したら三角形でないと判定されること()
-        {
-            var triangle = new Triangle(1, 1, 2);
-            Assert.That(triangle.GetTypeOfTriangle(), Is.EqualTo(TriangleType.Invalid));
         }
 
         [Test]
