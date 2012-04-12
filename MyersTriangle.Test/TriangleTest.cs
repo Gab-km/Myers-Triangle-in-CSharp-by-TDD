180274a45eb4438c07aa90389026685b804c2892
Gab_km <the25thcromosome@gmail.com>
2012-04-12 23:22:48 +0900

    辺1と3が等しく三角形でないテストケースを追加。
    diff --git a/MyersTriangle.Test/TriangleTest.cs b/MyersTriangle.Test/TriangleTest.cs
    index f1e0951..d7f2079 100644
    --- a/MyersTriangle.Test/TriangleTest.cs
    +++ b/MyersTriangle.Test/TriangleTest.cs
    @@ -19,6 +19,7 @@ namespace MyersTriangle.Test
             [TestCase(1, 2, 0, TriangleType.Invalid)]
             [TestCase(3, 0, 5, TriangleType.Invalid)]
             [TestCase(1, 1, 2, TriangleType.Invalid)]
    +        [TestCase(2, 4, 2, TriangleType.Invalid)]
             [TestCase(3, 6, 2, TriangleType.Invalid)]
             [TestCase(-1, 4, 5, TriangleType.Invalid)]
             public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)

--- MyersTriangle.Test/TriangleTest.cs
+++ MyersTriangle.Test/TriangleTest.cs
@@ -19,6 +19,7 @@ namespace MyersTriangle.Test
         [TestCase(1, 2, 0, TriangleType.Invalid)]
         [TestCase(3, 0, 5, TriangleType.Invalid)]
         [TestCase(1, 1, 2, TriangleType.Invalid)]
+        [TestCase(2, 4, 2, TriangleType.Invalid)]
         [TestCase(3, 6, 2, TriangleType.Invalid)]
         [TestCase(-1, 4, 5, TriangleType.Invalid)]
         public void 三角形を判定する(int side1, int side2, int side3, TriangleType resutl)
